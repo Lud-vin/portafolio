dae5900652ddc5e32424d81b3abc5e74bba76257
Ludvin F. <ludwing.df@live.com>
2025-06-03 00:50:25 -0600

    feat: Enhance project descriptions in ProjectSection for better clarity and user engagement

--- src/components/projects/ProjectSection.jsx
+++ src/components/projects/ProjectSection.jsx
@@ -1,82 +1,99 @@
 import ProjectItem from "./ProjectItem";
 
 export default function ProjectSection() {
-    const projects = [
-        {
-            link: "https://lemon-front.netlify.app/",
-            codelink: 'https://github.com/Ludvin7x/littlelemon-api',
-            imgPath: "project7.png",
-            name: "Lemon Restaurant Front-end",
-            description: `A modern restaurant website with an intuitive menu and ordering experience.\nUsername: customer, password: test1234`,
-            techStacks: ["React","Bootstrap" , "JavaScript", "CSS", "Vite"]
-        },
-        {
-            link:"https://little-lemon-api-yexw.onrender.com/admin",
-            codelink: "https://github.com/Ludvin7x/littlelemon-api",    
-            imgPath: "project3.png",
-            name: "Lemon Restaurant Back-end",
-            description: "A RESTful API for managing restaurant data, including menu items, orders, users.",
-            techStacks: ["Django", "Python", "MySQL", "JWT","Sqlite"]
-        },
-        {
-            link: "https://ludwingdf.wixsite.com/ayleen",
-            codelink: "https://github.com/Ludvin7x/Ayleen-Restaurant",
-            imgPath: "project5.png",
-            name: "Ayleen Restaurant",
-            description: "Web-based restaurant ordering system with a user-friendly interface for easy navigation and ordering.",
-            techStacks: ["React"]
-        },
-        {
-            link: "https://gestion-empleados-api.onrender.com",
-            codelink: "https://github.com/Ludvin7x/empleados-Backend",
-            imgPath: "project2.png",
-            name: "Employee Management Back-end",
-            description: "API for managing employee data, including CRUD operations",
-            techStacks: ["Flask-Restx", "SqLite", "Python"]
-        },
-        {
-            link: "https://empleados-frontend.netlify.app/",
-            codelink: "https://github.com/Ludvin7x/empleados-Backend",
-            imgPath: "project4.png",
-            name: "Employee Management Front-end",
-            description: "Web application for managing employee data, including CRUD operations.",
-            techStacks: ["Angular", "TypeScript", "Bootstrap"]
-        },
-        {
-            link: "https://github.com/Ludvin7x/Control-visitas",
-            codelink: "https://github.com/Ludvin7x/Control-visitas",
-            imgPath: "project6.png",
-            name: "Visitor Management App",
-            description: "Android app for managing visitor access, notifications, and reporting for both residents and admins.",
-            techStacks: ["Kotlin", "Android", "Firebase", "NoSQL"]
-        },
-        {
-            link: "https://ludvin7x-portfolio.netlify.app/",
-            codelink: "https://github.com/Ludvin7x/portafolio",
-            imgPath: "project1.png",
-            name: "📜 Portfolio 💼",
-            description: "My personal portfolio showcasing my projects, skills, and experience.",
-            techStacks: ["React", "TailwindCSS"]
-        },
-    ];
-
-    return (
+  const projects = [
+    {
+      link: "https://lemon-front.netlify.app/",
+      codelink: "https://github.com/Ludvin7x/littlelemon-api",
+      imgPath: "project7.png",
+      name: "Lemon Restaurant Front-end",
+      description: (
         <>
-            <h3 className="text-center text-3xl text-gray-200 font-semibold">Projects</h3>
-            <div className="grid grid-cols-1 gap-10 pt-12 pb-20 xl:w-3/4 2xs:w-full lg:w-5/6 mx-auto">
-                {projects.map((project, index) => (
-                    <ProjectItem
-                        key={index}
-                        link={project.link}
-                        imgPath={project.imgPath}
-                        name={project.name}
-                        description={project.description}
-                        codeLink={project.codelink}
-                        techStacks={project.techStacks}
-                    />
-                ))}
-            </div>
-            <hr className="border-gray-700 mt-10" />
+          A modern restaurant website featuring an intuitive menu, a seamless
+          ordering experience with a **shopping cart system**, and robust **user
+          registration**. You can **create your own account or use the provided
+          demo credentials**.
+          <br />
+          <br />
+          **Demo Account:** Username: customer, Password: test1234
         </>
-    );
+      ),
+      techStacks: ["React", "Bootstrap", "JavaScript", "CSS", "Vite"],
+    },
+    {
+      link: "https://little-lemon-api-yexw.onrender.com/admin",
+      codelink: "https://github.com/Ludvin7x/littlelemon-api",
+      imgPath: "project3.png",
+      name: "Lemon Restaurant Back-end",
+      description:
+        "A RESTful API for managing restaurant data, including menu items, orders, users.",
+      techStacks: ["Django", "Python", "MySQL", "JWT", "Sqlite"],
+    },
+    {
+      link: "https://ludwingdf.wixsite.com/ayleen",
+      codelink: "https://github.com/Ludvin7x/Ayleen-Restaurant",
+      imgPath: "project5.png",
+      name: "Ayleen Restaurant",
+      description:
+        "Web-based restaurant ordering system with a user-friendly interface for easy navigation and ordering.",
+      techStacks: ["React"],
+    },
+    {
+      link: "https://gestion-empleados-api.onrender.com",
+      codelink: "https://github.com/Ludvin7x/empleados-Backend",
+      imgPath: "project2.png",
+      name: "Employee Management Back-end",
+      description: "API for managing employee data, including CRUD operations",
+      techStacks: ["Flask-Restx", "SqLite", "Python"],
+    },
+    {
+      link: "https://empleados-frontend.netlify.app/",
+      codelink: "https://github.com/Ludvin7x/empleados-Backend",
+      imgPath: "project4.png",
+      name: "Employee Management Front-end",
+      description:
+        "Web application for managing employee data, including CRUD operations.",
+      techStacks: ["Angular", "TypeScript", "Bootstrap"],
+    },
+    {
+      link: "https://github.com/Ludvin7x/Control-visitas",
+      codelink: "https://github.com/Ludvin7x/Control-visitas",
+      imgPath: "project6.png",
+      name: "Visitor Management App",
+      description:
+        "Android app for managing visitor access, notifications, and reporting for both residents and admins.",
+      techStacks: ["Kotlin", "Android", "Firebase", "NoSQL"],
+    },
+    {
+      link: "https://ludvin7x-portfolio.netlify.app/",
+      codelink: "https://github.com/Ludvin7x/portafolio",
+      imgPath: "project1.png",
+      name: "📜 Portfolio 💼",
+      description:
+        "My personal portfolio showcasing my projects, skills, and experience.",
+      techStacks: ["React", "TailwindCSS"],
+    },
+  ];
+
+  return (
+    <>
+      <h3 className="text-center text-3xl text-gray-200 font-semibold">
+        Projects
+      </h3>
+      <div className="grid grid-cols-1 gap-10 pt-12 pb-20 xl:w-3/4 2xs:w-full lg:w-5/6 mx-auto">
+        {projects.map((project, index) => (
+          <ProjectItem
+            key={index}
+            link={project.link}
+            imgPath={project.imgPath}
+            name={project.name}
+            description={project.description}
+            codeLink={project.codelink}
+            techStacks={project.techStacks}
+          />
+        ))}
+      </div>
+      <hr className="border-gray-700 mt-10" />
+    </>
+  );
 }
